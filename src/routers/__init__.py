33f87a13148d2b79b7ceafc44757db9fce2e1900
timur199429 <timur199429@gmail.com>
2025-03-26 20:46:33 +0300

    add clickback router to main router

--- src/routers/__init__.py
+++ src/routers/__init__.py
@@ -2,9 +2,12 @@ from fastapi import APIRouter
 
 from src.routers.nutra import nutra_router
 from src.routers.vitrina import vitrina_router
+from src.routers.clickback import clickback
 
 
 main_router = APIRouter()
 main_router.include_router(nutra_router)
 main_router.include_router(vitrina_router)
+main_router.include_router(clickback)
+
 
